fdb2d2a4320f7a6051b68d886fff0a5f125b0707
zhaokai <1005324930@qq.com>
2018-08-04 22:46:14 +0800

    feat: 添加子菜单

--- MainWindow.py
+++ MainWindow.py
@@ -3,7 +3,7 @@ import sys
 
 # pylint: disable=E0611
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QMenu
 from PyQt5.QtGui import QIcon, QKeySequence
 
 class ExampleMainWindow(QMainWindow):
@@ -15,21 +15,26 @@ class ExampleMainWindow(QMainWindow):
     
     def initUI(self):
 
-        exitAct = QAction(QIcon("exit.png"), "&Exit", self)
-        exitAct.setShortcut(QKeySequence.Quit)
-        # exitAct.setShortcut("Ctrl+Q")
+        exitAct = QAction(QIcon("exit.png"), "Exit", self)
+        # exitAct.setShortcut(QKeySequence.Quit) # windows not support
+        exitAct.setShortcut("Ctrl+Q")
         # exitAct.setShortcut(Qt.CTRL + Qt.Key_Q)
         exitAct.setStatusTip("Exit application")
         exitAct.triggered.connect(self.close)
 
+        emailMenu = QMenu('&Email', self)
+        gmailMenu = QAction(QIcon("gmail.png"), '&Gmail', self)
+        emailMenu.addAction(gmailMenu)
+
         menubar = self.menuBar()
         fileMenu = menubar.addMenu("&File")
+        fileMenu.addMenu(emailMenu)
         fileMenu.addAction(exitAct)
 
         self.statusBar().showMessage("Ready")
 
         self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Simple menu')
+        self.setWindowTitle('Submenu')
         self.show()
     
 if __name__ == '__main__':
