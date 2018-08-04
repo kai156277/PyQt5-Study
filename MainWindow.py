57078267785fa95e6551c8f6e93afcec52fd20cf
zhaokai <1005324930@qq.com>
2018-08-04 21:42:12 +0800

    feat: 添加菜单栏

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+
+# pylint: disable=E0611
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction
+from PyQt5.QtGui import QIcon, QKeySequence
 
 class ExampleMainWindow(QMainWindow):
 
@@ -11,10 +15,21 @@ class ExampleMainWindow(QMainWindow):
     
     def initUI(self):
 
+        exitAct = QAction(QIcon("exit.png"), "&Exit", self)
+        exitAct.setShortcut(QKeySequence.Quit)
+        # exitAct.setShortcut("Ctrl+Q")
+        # exitAct.setShortcut(Qt.CTRL + Qt.Key_Q)
+        exitAct.setStatusTip("Exit application")
+        exitAct.triggered.connect(self.close)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu("&File")
+        fileMenu.addAction(exitAct)
+
         self.statusBar().showMessage("Ready")
 
         self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Statusbar')
+        self.setWindowTitle('Simple menu')
         self.show()
     
 if __name__ == '__main__':
