2457cef6ae0b9e91a3db712d0102496ddad74008
zhaokai <1005324930@qq.com>
2018-08-04 14:37:51 +0800

    feat: 添加图标

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,25 @@
 import sys
-from PyQt5 import QtWidgets
+# pylint: disable = E0611
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon
 
-def window():
-    app = QtWidgets.QApplication(sys.argv)
-    w = QtWidgets.QWidget()
-    b = QtWidgets.QLabel(w)
-    b.setText("Hello World!")
-    w.setGeometry(100, 100, 200, 50)
-    b.move(50, 20)
-    w.setWindowTitle("PyQt5")
-    w.show()
-    sys.exit(app.exec_())
+class HelloWorld(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    
+    def initUI(self):
+        b = QLabel(self)
+        b.setText("Hello World!")
+        self.setGeometry(100, 100, 200, 50)
+        b.move(50, 20)
+        self.setWindowTitle("PyQt5")
+        self.setWindowIcon(QIcon('google.png'))
+        self.show()
 
 if __name__ == '__main__':
-    window()
+    app = QApplication(sys.argv)
+    hello = HelloWorld()
+    sys.exit(app.exec_())
