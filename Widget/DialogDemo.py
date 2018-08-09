352bc3ffeec2f4fdef9e4e09957c288cc2dbd1fb
zhaokai <1005324930@qq.com>
2018-08-09 23:16:26 +0800

    feat: 添加Messagebox

--- Widget/DialogDemo.py
+++ Widget/DialogDemo.py
@@ -7,11 +7,25 @@ from importQt import *
 class DialogDemo(QWidget):
     def __init__(self):
         super().__init__()
+        layout = QHBoxLayout()
         self.setWindowTitle("Dialog Demo")
 
         self.btn = QPushButton(self)
         self.btn.setText("Dialog")
         self.btn.clicked.connect(self.showDialog)
+        layout.addWidget(self.btn)
+
+        self.msgBox = QPushButton(self)
+        self.msgBox.setText("Message Box")
+        self.msgBox.clicked.connect(self.showMessageBox)
+        layout.addWidget(self.msgBox)
+
+        self.setLayout(layout)
+
+    def showMessageBox(self):
+        reply = QMessageBox.information(self, "Info", "Info Message",
+                                        QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+        print(reply)
 
     def showDialog(self):
         dialog = QDialog()
